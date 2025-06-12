75cb16e52fbd7e51f9a6e981dbae6bd3334afc9e
SKYZ by Zainab Khalid <skyz.zk.official@gmail.com>
2025-06-12 15:13:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+let body=document.querySelector("body");
+let hey=document.querySelector(".in");
+let buttonA=document.querySelector(".a");
+buttonA.addEventListener("click", () => {
+    hey.innerHTML="When you're happy, you glow different.";
+    hey.style.fontSize="50px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="pink";
+
+})
+let buttonB=document.querySelector(".b");
+buttonB.addEventListener("click", () => {
+    hey.innerHTML="Sadness teaches us what joy truly feels like. Without the rain, we’d forget the beauty of the sun.";
+    hey.style.fontSize="35px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="lightyellow";
+
+})
+let buttonC=document.querySelector(".c");
+buttonC.addEventListener("click", () => {
+    hey.innerHTML="It’s okay to feel lost sometimes. Stars can’t shine without a little darkness";
+    hey.style.fontSize="35px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="violet";
+
+
+})
+let buttonD=document.querySelector(".d");
+buttonD.addEventListener("click", () => {
+    hey.innerHTML="You are not broken, you are hurting. And healing takes time — even stars burn quietly for millions of years before they shine.";
+    hey.style.fontSize="35px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="lightblue";
+
+})
+let buttonE=document.querySelector(".e");
+buttonE.addEventListener("click", () => {
+    hey.innerHTML="Even on my hardest days, I find one small thing to be thankful for — and that keeps me going.";
+    hey.style.fontSize="35px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="lightgreen";
+
+})
+let buttonF=document.querySelector(".f");
+buttonF.addEventListener("click", () => {
+    hey.innerHTML="Crying isn’t a sign of weakness. It’s the soul's way of washing away the weight it can no longer carry";
+    hey.style.fontSize="35px";
+    hey.style.textAlign="center";
+    hey.style.paddingTop="25px";
+    body.style.backgroundColor="orange";
+
+
+})
